036314538b079f84f73f5b11d78d462f7c675649
Li Shunan <lishunan246@gmail.com>
2016-10-17 21:08:14 +0800

    f:

--- LineSegment.py
+++ LineSegment.py
@@ -1,8 +1,17 @@
+# coding=utf-8
+import numpy
+import math
+
+
 class LineSegment:
     def __init__(self, x1, y1, x2, y2):
+        if y1 > y2:
+            x1, y1, x2, y2 = x2, y2, x1, y1  # y1在下 y2在上
+
         self.x1 = x1
         self.x2 = x2
         self.y1 = y1
         self.y2 = y2
-        self.k = (y1 - y2) / (1.0*(x1 - x2))
+        self.k = (y1 - y2) / (1.0 * (x1 - x2)) if x1 != x2 else float('nan')
         self.b = y1 - self.k * x1
+        self.theta = numpy.arctan(self.k) if x1 != x2 else math.pi / 2
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 # coding=utf-8
+
 import numpy as np
 import cv2
+import os
+import math
 from LineSegment import LineSegment
 
+
+# 条形码比例3:2
 
 # 控制台输出单击处的颜色
 # noinspection PyUnusedLocal
@@ -15,55 +20,63 @@ lower_blue = np.array([100, 20, 20])
 upper_blue = np.array([125, 255, 255])
 kernel = np.ones((2, 2), np.uint8)
 
-img = cv2.imread('input//6.jpg', cv2.IMREAD_COLOR)
-cv2.imshow('image', img)
+input_path = 'input//'
+output_path = 'output//'
 
-height, width, depth = img.shape
-cv2.setMouseCallback('Image', on_mouse_click, img)
+for filename in os.listdir(input_path):
 
-while True:
-    key = 0xff & cv2.waitKey()
-    if key == 27:
-        break
-    elif key == ord('r'):
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        mask = img.copy()
-        mask = cv2.inRange(img, lower_blue, upper_blue)
-        mask = cv2.bitwise_not(mask)
-        cv2.imshow("mask", mask)
+    img = cv2.imread(input_path + filename, cv2.IMREAD_COLOR)
+    height, width, depth = img.shape
 
-        res = cv2.bitwise_and(img, img, mask=mask)
-        res = cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
-        # cv2.imshow('paint', res)
-        res = cv2.inpaint(res, cv2.dilate(cv2.bitwise_not(mask), kernel, iterations=1), 3, cv2.INPAINT_TELEA)
-        # cv2.imshow('inpaint', res)
-    elif key == ord('g'):
-        edges = cv2.Canny(img, 50, 150, apertureSize=3)
-        lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 100, minLineLength=100, maxLineGap=10)
-        line_group = []
-        for line in lines:
-            x1, y1, x2, y2 = line[0]
-            ls = LineSegment(x1, y1, x2, y2)
-            if len(line_group) == 0:  # first line group
+    gravy = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gravy, 50, 150, apertureSize=3)
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 100, minLineLength=100, maxLineGap=10)
+    line_group = []
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        ls = LineSegment(x1, y1, x2, y2)
+        if len(line_group) == 0:  # first line group
+            line_group.append([ls])
+        else:
+            done = False
+            for i in range(0, len(line_group)):
+                if abs(line_group[i][0].theta - ls.theta) < 0.05:  # 比较斜率
+                    line_group[i].append(ls)
+                    done = True
+                    break
+
+            if not done:
                 line_group.append([ls])
-            else:
-                done = False
-                for i in range(0, len(line_group)):
-                    if abs(np.arctan(line_group[i][0].k) - np.arctan(ls.k)) < 0.05:  # 比较斜率
-                        line_group[i].append(ls)
-                        done = True
-                        break
 
-                if not done:
-                    line_group.append([ls])
+    max_group = max(line_group, key=lambda x: len(x))
 
-        max_group = max(line_group, key=lambda x: len(x))
-        for ls in max_group:
-            x1, y1, x2, y2 = ls.x1, ls.y1, ls.x2, ls.y2
-            cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-        cv2.imshow('houghlines5.jpg', img)
-    elif key == ord('f'):
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        mask = cv2.inRange(hsv, np.array([]), np.array([]))
+    max_group.sort(key=lambda x: x.x1)
 
+    black = np.zeros((height, width, depth), np.uint8)
+
+    for ls in max_group:
+        x1, y1, x2, y2 = ls.x1, ls.y1, ls.x2, ls.y2
+        cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.line(black, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+    x_max = max_group[0].x1
+    x_min = max_group[len(max_group) - 1].x1
+
+    y1s = map(lambda l: l.y1, max_group)
+    y_min = min(y1s)
+
+    y_max = max(map(lambda l: l.y2, max_group))
+
+    cv2.line(img, (x_max, y_max), (x_max, y_min), (255, 0, 0), 2)
+    cv2.line(img, (x_min, y_max), (x_min, y_min), (255, 0, 0), 2)
+    cv2.line(img, (x_max, y_min), (x_min, y_min), (255, 0, 0), 2)
+    cv2.line(img, (x_min, y_max), (x_max, y_max), (255, 0, 0), 2)
+
+    cv2.line(black, (x_max, y_max), (x_max, y_min), (0, 0, 255), 2)
+    cv2.line(black, (x_min, y_max), (x_min, y_min), (0, 0, 255), 2)
+    cv2.line(black, (x_max, y_min), (x_min, y_min), (0, 0, 255), 2)
+    cv2.line(black, (x_min, y_max), (x_max, y_max), (0, 0, 255), 2)
+
+    cv2.imwrite(output_path + 'line_' + filename, img)
+    cv2.imwrite(output_path + 'only_line_' + filename, black)
 cv2.destroyAllWindows()
