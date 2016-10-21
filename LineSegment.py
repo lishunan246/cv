4c75726364a019bea44e60814bf1d32774ce6057
Li Shunan <lishunan246@gmail.com>
2016-10-21 23:54:01 +0800

    update grouping method

--- LineSegment.py
+++ LineSegment.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 import numpy
 import math
+from decimal import *
 
 
 class LineSegment:
@@ -13,22 +14,70 @@ class LineSegment:
         self.y1 = y1
         self.y2 = y2
         self.k = (y1 - y2) / (1.0 * (x1 - x2)) if x1 != x2 else float('nan')
-        self.b = y1 - self.k * x1
-        self.theta = math.atan2(y1-y2,x1-x2)
-        self.d = -self.b / self.k * numpy.sin(self.theta) if self.k != 0 else x1  # 到直线到原点的最近距离
-    def update(self,theta):
-        self.theta=theta
-        x=(self.x1+self.x2)/2.0
-        y=(self.y1+self.y2)/2.0
-        #self.d=numpy.sqrt(x*x+y*y)*numpy.sin(self.theta)
-        self.d=x
+        self.theta = math.atan2(y1 - y2, x1 - x2)
+
+        self.b = y1 - self.k * x1 if x1 != x2 else float('nan')
+
+        # 到直线到原点的最近距离
+        #self.d = -self.b * (numpy.sin(self.theta) / math.tan(self.theta)) if not math.isnan(self.k) else x1
+
+    def update(self, theta):
+        old_theta = self.theta
+        self.theta = theta
+        x = (self.x1 + self.x2) / 2.0
+        y = (self.y1 + self.y2) / 2.0
+
+        d_theta = self.theta - old_theta
+
+        p1 = numpy.array([self.x1, self.y1, 1])
+        p2 = numpy.array([self.x2, self.y2, 1])
+        translate1 = numpy.array([
+            [1, 0, 0],
+            [0, 1, 0],
+            [x - self.x1, y - self.y1, 1]
+        ])
+
+        translate2 = numpy.array([
+            [1, 0, 0],
+            [0, 1, 0],
+            [x - self.x2, y - self.y2, 1]
+        ])
+        p1 = p1.dot(translate1)
+        p2 = p2.dot(translate2)
+        rotate = numpy.array([
+            [numpy.cos(d_theta), numpy.sin(d_theta), 0],
+            [-numpy.sin(d_theta), numpy.cos(d_theta), 0],
+            [0, 0, 1]
+        ])
+        p1 = p1.dot(rotate).dot(numpy.array([
+            [1, 0, 0],
+            [0, 1, 0],
+            [self.x1 - x, self.y1 - y, 1]
+        ]))
+
+        p2 = p2.dot(rotate).dot(numpy.array([
+            [1, 0, 0],
+            [0, 1, 0],
+            [self.x2 - x, self.y2 - y, 1]
+        ]))
+        self.x1,self.y1 ,t = p1.astype(int)
+        self.x2, self.y2, t = p2.astype(int)
+
+        # self.d = Decimal(y) - Decimal(math.tan(theta)) * Decimal(x) if theta != math.pi / 2 else x
+        p1 = (numpy.array([[self.x1, self.y1]])).astype(float)
+        p2 = numpy.array([[self.x2, self.y2]]).astype(float)
+        p0 = numpy.array([[0, 0]]).astype(float)
+
+        self.d = (numpy.cross(p0 - p1, p0 - p2)**2).sum() / (1.0 * ((p1 - p2)**2) .sum())
+        #self.d = x
+        self.d=numpy.sqrt(self.d)
 
 class LineSegmentGroup:
     def __init__(self):
         self.d_max = None
         self.d_min = None
         self.lines = []
-        self.D = 65
+        self.D = 80
 
     def insert(self, ls):
         if self.d_max is None:
@@ -43,4 +92,8 @@ class LineSegmentGroup:
 
     def is_near(self, ls):
         distance = min(abs(ls.d - self.d_min), abs(ls.d - self.d_max))
-        return distance < self.D
+        if distance < self.D:
+            return True
+        else:
+            #print distance, ls.d
+            return False
--- main.py
+++ main.py
@@ -2,29 +2,22 @@
 
 import numpy as np
 import cv2
+import math
 import os
 from LineSegment import LineSegment, LineSegmentGroup
 
 
 # 条形码比例3:2
 
-# 控制台输出单击处的颜色
-# noinspection PyUnusedLocal
-def on_mouse_click(event, x, y, flags, frame):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        print frame[y, x]
-
-
 yellow = (0, 255, 255)
 thickness = 10
-
 font = cv2.FONT_HERSHEY_SIMPLEX
 input_path = 'input//'
 output_path = 'output//'
 
 for filename in os.listdir(input_path):
-
-    img = cv2.imread(input_path + filename, cv2.IMREAD_COLOR)
+    original = cv2.imread(input_path + filename, cv2.IMREAD_COLOR)
+    img = original.copy()
     height, width, depth = img.shape
 
     gravy = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -42,7 +35,7 @@ for filename in os.listdir(input_path):
         else:
             done = False
             for i in range(0, len(line_group)):
-                if abs(line_group[i][0].theta - ls.theta) < 0.3:  # 比较斜率
+                if abs(line_group[i][0].theta - ls.theta) < 0.1:  # 比较斜率
                     line_group[i].append(ls)
                     done = True
                     break
@@ -54,40 +47,61 @@ for filename in os.listdir(input_path):
 
     avg_theta = sum([ls.theta for ls in max_group]) / float(len(max_group))
 
-
-
     for ls in max_group:
-        x1,x2,y1,y2=ls.x1,ls.x2,ls.y1,ls.y2
-        cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 3)
-        cv2.line(black, (x1, y1), (x2, y2), (0, 0, 255), 3)
+        x1, x2, y1, y2 = ls.x1, ls.x2, ls.y1, ls.y2
+        # cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 3)
+        # cv2.line(black, (x1, y1), (x2, y2), (0, 0, 255), 3)
         # 根据斜率找到的线段
         ls.update(avg_theta)
 
     max_group.sort(key=lambda x: x.d)
     group_d = []
 
+    black2 = np.zeros((height, width, depth), np.uint8)
     for i in range(0, len(max_group)):
         ls = max_group[i]
-        found_group = False
-        for j in range(0, len(group_d)):
-            if group_d[j].is_near(ls):
-                group_d[j].insert(ls)
-                found_group = True
-                break
-        if not found_group:
+        x1, x2, y1, y2 = ls.x1, ls.x2, ls.y1, ls.y2
+        cv2.line(black2, (x1, y1), (x2, y2), (0, 0, 255), 3)
+        #cv2.waitKey()
+        #cv2.imshow("lines", cv2.resize(black2,(0,0),fx=0.2,fy=0.2))
+
+        j = len(group_d) - 1
+        if j < 0:
             new_g = LineSegmentGroup()
             new_g.insert(ls)
             group_d.append(new_g)
+        else:
+            if group_d[j].is_near(ls):
+                group_d[j].insert(ls)
+            else:
+                new_g = LineSegmentGroup()
+                new_g.insert(ls)
+                group_d.append(new_g)
 
     cv2.putText(black, 'Group by d: ' + str(len(group_d)), (40, 200), font, 2, (255, 255, 255), 2)
 
     max_group = max(group_d, key=lambda x: len(x.lines)).lines
+    for ls in max_group:
+        x1, x2, y1, y2 = ls.x1, ls.x2, ls.y1, ls.y2
+        cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 3)
+        cv2.line(black, (x1, y1), (x2, y2), (0, 0, 255), 3)
+        # 根据斜率 & x找到的线段
+        # ls.update(avg_theta)
 
     x_max = max_group[0].x1
     x_min = max_group[len(max_group) - 1].x1
 
     y_min = min(map(lambda l: l.y1, max_group))
     y_max = max(map(lambda l: l.y2, max_group))
+
+    x_avg = (x_max + x_min) / 2
+    y_avg = (y_max + y_min) / 2
+
+    pts1 = np.float32([[x_min, y_min], [x_max, y_min], [x_min, y_max], [x_max, y_max]])
+    pts2 = np.float32([[0, 0], [300, 0], [0, 200], [300, 200]])
+    M = cv2.getPerspectiveTransform(pts1, pts2)
+
+    dst = cv2.warpPerspective(original, M, (300, 200))
 
     cv2.line(img, (x_max, y_max), (x_max, y_min), yellow, thickness)
     cv2.line(img, (x_min, y_max), (x_min, y_min), yellow, thickness)
@@ -101,4 +115,5 @@ for filename in os.listdir(input_path):
 
     cv2.imwrite(output_path + 'line_' + filename, img)
     cv2.imwrite(output_path + 'only_line_' + filename, black)
+    cv2.imwrite(output_path + 'dst_' + filename, dst)
 cv2.destroyAllWindows()
