4b536d93296dfd76eb7d5f6a47a8d6bfd2adbc61
Li Shunan <lishunan246@gmail.com>
2016-10-22 18:39:18 +0800

    现在可以处理倾斜图像

--- LineSegment.py
+++ LineSegment.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 import numpy
 import math
-from decimal import *
+import cv2
 
 
 class LineSegment:
@@ -13,64 +13,55 @@ class LineSegment:
         self.x2 = x2
         self.y1 = y1
         self.y2 = y2
-        self.k = (y1 - y2) / (1.0 * (x1 - x2)) if x1 != x2 else float('nan')
-        self.theta = math.atan2(y1 - y2, x1 - x2)
 
+        self.y = (self.y1 + self.y2) / 2.0
+        self.x = (self.x1 + self.x2) / 2.0
+
+        self.k = (y1 - y2) / (1.0 * (x1 - x2)) if x1 != x2 else float('nan')
         self.b = y1 - self.k * x1 if x1 != x2 else float('nan')
 
+        self.theta = math.atan2(y1 - y2, x1 - x2)
+        self.d = float('nan')
         # 到直线到原点的最近距离
-        #self.d = -self.b * (numpy.sin(self.theta) / math.tan(self.theta)) if not math.isnan(self.k) else x1
+
+    def distance(self, x, y):
+        p1 = (numpy.array([[self.x1, self.y1]])).astype(float)
+        p2 = numpy.array([[self.x2, self.y2]]).astype(float)
+        p0 = numpy.array([[x, y]]).astype(float)
+
+        t = (numpy.cross(p0 - p1, p0 - p2) ** 2).sum() / (1.0 * ((p1 - p2) ** 2).sum())
+        return numpy.sqrt(t)
 
     def update(self, theta):
-        old_theta = self.theta
+        d_theta = theta - self.theta
         self.theta = theta
-        x = (self.x1 + self.x2) / 2.0
-        y = (self.y1 + self.y2) / 2.0
 
-        d_theta = self.theta - old_theta
-
-        p1 = numpy.array([self.x1, self.y1, 1])
-        p2 = numpy.array([self.x2, self.y2, 1])
+        p1 = numpy.array([self.x1, self.y1, 1]).astype(float)
+        p2 = numpy.array([self.x2, self.y2, 1]).astype(float)
         translate1 = numpy.array([
             [1, 0, 0],
             [0, 1, 0],
-            [x - self.x1, y - self.y1, 1]
+            [-self.x, -self.y, 1]
         ])
-
-        translate2 = numpy.array([
-            [1, 0, 0],
-            [0, 1, 0],
-            [x - self.x2, y - self.y2, 1]
-        ])
-        p1 = p1.dot(translate1)
-        p2 = p2.dot(translate2)
         rotate = numpy.array([
             [numpy.cos(d_theta), numpy.sin(d_theta), 0],
             [-numpy.sin(d_theta), numpy.cos(d_theta), 0],
             [0, 0, 1]
         ])
-        p1 = p1.dot(rotate).dot(numpy.array([
+        translate2 = numpy.array([
             [1, 0, 0],
             [0, 1, 0],
-            [self.x1 - x, self.y1 - y, 1]
-        ]))
+            [self.x, self.y, 1]
+        ])
 
-        p2 = p2.dot(rotate).dot(numpy.array([
-            [1, 0, 0],
-            [0, 1, 0],
-            [self.x2 - x, self.y2 - y, 1]
-        ]))
-        self.x1,self.y1 ,t = p1.astype(int)
+        p1 = p1.dot(translate1).dot(rotate).dot(translate2)
+        p2 = p2.dot(translate1).dot(rotate).dot(translate2)
+
+        self.x1, self.y1, t = p1.astype(int)
         self.x2, self.y2, t = p2.astype(int)
 
-        # self.d = Decimal(y) - Decimal(math.tan(theta)) * Decimal(x) if theta != math.pi / 2 else x
-        p1 = (numpy.array([[self.x1, self.y1]])).astype(float)
-        p2 = numpy.array([[self.x2, self.y2]]).astype(float)
-        p0 = numpy.array([[0, 0]]).astype(float)
+        self.d = self.distance(0, 0)
 
-        self.d = (numpy.cross(p0 - p1, p0 - p2)**2).sum() / (1.0 * ((p1 - p2)**2) .sum())
-        #self.d = x
-        self.d=numpy.sqrt(self.d)
 
 class LineSegmentGroup:
     def __init__(self):
@@ -95,5 +86,5 @@ class LineSegmentGroup:
         if distance < self.D:
             return True
         else:
-            #print distance, ls.d
+            # print distance, ls.d
             return False
--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import math
 import os
 from LineSegment import LineSegment, LineSegmentGroup
 
-
 # 条形码比例3:2
 
 yellow = (0, 255, 255)
@@ -28,8 +27,8 @@ for filename in os.listdir(input_path):
     for line in lines:
         x1, y1, x2, y2 = line[0]
         ls = LineSegment(x1, y1, x2, y2)
-        cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-        cv2.line(black, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 1)
+        cv2.line(black, (x1, y1), (x2, y2), (0, 255, 0), 1)
         if len(line_group) == 0:  # first line group
             line_group.append([ls])
         else:
@@ -48,9 +47,6 @@ for filename in os.listdir(input_path):
     avg_theta = sum([ls.theta for ls in max_group]) / float(len(max_group))
 
     for ls in max_group:
-        x1, x2, y1, y2 = ls.x1, ls.x2, ls.y1, ls.y2
-        # cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 3)
-        # cv2.line(black, (x1, y1), (x2, y2), (0, 0, 255), 3)
         # 根据斜率找到的线段
         ls.update(avg_theta)
 
@@ -62,8 +58,6 @@ for filename in os.listdir(input_path):
         ls = max_group[i]
         x1, x2, y1, y2 = ls.x1, ls.x2, ls.y1, ls.y2
         cv2.line(black2, (x1, y1), (x2, y2), (0, 0, 255), 3)
-        #cv2.waitKey()
-        #cv2.imshow("lines", cv2.resize(black2,(0,0),fx=0.2,fy=0.2))
 
         j = len(group_d) - 1
         if j < 0:
@@ -86,7 +80,57 @@ for filename in os.listdir(input_path):
         cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 3)
         cv2.line(black, (x1, y1), (x2, y2), (0, 0, 255), 3)
         # 根据斜率 & x找到的线段
-        # ls.update(avg_theta)
+
+    line_min = min(max_group, key=lambda x: x.d)
+    line_max = max(max_group, key=lambda x: x.d)
+
+    x_avg = sum([ls.x for ls in max_group]) / (1.0 * len(max_group))
+    y_avg = sum([ls.y for ls in max_group]) / (1.0 * len(max_group))
+    xx = x_avg + 100
+    yy = y_avg + 100 * math.tan(line_min.theta)
+
+    new_line = LineSegment(x_avg, y_avg, xx, yy)
+    new_line.update(line_min.theta)
+    delta_d = (line_min.d + line_max.d) / 2.0 - new_line.d
+
+    x_avg += delta_d * math.cos(line_min.theta + math.pi / 2.0)
+    y_avg += delta_d * math.sin(line_min.theta + math.pi / 2.0)
+
+    cv2.circle(black, (int(x_avg), int(y_avg)), 30, (255, 255, 255), -1)
+
+    w = line_max.distance(line_min.x, line_min.y)
+    h = w / 3.0 * 2.4
+    r_2 = (w ** 2 + h ** 2) / 4.0
+
+    l = line_min
+    x1, x2, y1, y2 = l.x1, l.x2, l.y1, l.y2
+    a = (y1 - y2) ** 2 + (x1 - x2) ** 2
+    b = 2.0 * ((y1 - y2) * (y1 - y_avg) + (x1 - x2) * (x1 - x_avg))
+    c = (y1 - y_avg) ** 2 + (x1 - x_avg) ** 2 - r_2
+
+    t1 = (-b - math.sqrt(b ** 2 - 4.0 * a * c)) / (2.0 * a)
+    t2 = (-b + math.sqrt(b ** 2 - 4.0 * a * c)) / (2.0 * a)
+
+    xn1 = x1 + t1 * (x1 - x2)
+    yn1 = y1 + t1 * (y1 - y2)
+
+    xn2 = x1 + t2 * (x1 - x2)
+    yn2 = y1 + t2 * (y1 - y2)
+
+    l = line_max
+    x1, x2, y1, y2 = l.x1, l.x2, l.y1, l.y2
+    a = (y1 - y2) ** 2 + (x1 - x2) ** 2
+    b = 2.0 * ((y1 - y2) * (y1 - y_avg) + (x1 - x2) * (x1 - x_avg))
+    c = (y1 - y_avg) ** 2 + (x1 - x_avg) ** 2 - r_2
+
+    t1 = (-b - math.sqrt(b ** 2 - 4.0 * a * c)) / (2.0 * a)
+    t2 = (-b + math.sqrt(b ** 2 - 4.0 * a * c)) / (2.0 * a)
+
+    xn3 = x1 + t1 * (x1 - x2)
+    yn3 = y1 + t1 * (y1 - y2)
+
+    xn4 = x1 + t2 * (x1 - x2)
+    yn4 = y1 + t2 * (y1 - y2)
 
     x_max = max_group[0].x1
     x_min = max_group[len(max_group) - 1].x1
@@ -94,24 +138,31 @@ for filename in os.listdir(input_path):
     y_min = min(map(lambda l: l.y1, max_group))
     y_max = max(map(lambda l: l.y2, max_group))
 
-    x_avg = (x_max + x_min) / 2
-    y_avg = (y_max + y_min) / 2
-
-    pts1 = np.float32([[x_min, y_min], [x_max, y_min], [x_min, y_max], [x_max, y_max]])
+    cv2.circle(black, (int(xn1), int(yn1)), 30, (255, 255, 255), -1)
+    cv2.circle(black, (int(xn2), int(yn2)), 30, (255, 255, 255), -1)
+    cv2.circle(black, (int(xn3), int(yn3)), 30, (255, 255, 255), -1)
+    cv2.circle(black, (int(xn4), int(yn4)), 30, (255, 255, 255), -1)
+    # 3 4
+    # 1 2
+    pts1 = np.float32([[xn1, yn1], [xn3, yn3], [xn2, yn2], [xn4, yn4]])
+    # pts1 = np.float32([[x_min, y_min], [x_max, y_min], [x_min, y_max], [x_max, y_max]])
     pts2 = np.float32([[0, 0], [300, 0], [0, 200], [300, 200]])
     M = cv2.getPerspectiveTransform(pts1, pts2)
 
     dst = cv2.warpPerspective(original, M, (300, 200))
 
-    cv2.line(img, (x_max, y_max), (x_max, y_min), yellow, thickness)
-    cv2.line(img, (x_min, y_max), (x_min, y_min), yellow, thickness)
-    cv2.line(img, (x_max, y_min), (x_min, y_min), yellow, thickness)
-    cv2.line(img, (x_min, y_max), (x_max, y_max), yellow, thickness)
+    xn1, yn1, xn3, yn3, xn2, yn2, xn4, yn4 = int(xn1), int(yn1), int(xn3), int(yn3), int(xn2), int(yn2), int(xn4), int(
+        yn4)
 
-    cv2.line(black, (x_max, y_max), (x_max, y_min), yellow, thickness)
-    cv2.line(black, (x_min, y_max), (x_min, y_min), yellow, thickness)
-    cv2.line(black, (x_max, y_min), (x_min, y_min), yellow, thickness)
-    cv2.line(black, (x_min, y_max), (x_max, y_max), yellow, thickness)
+    cv2.line(img, (xn1, yn1), (xn2, yn2), yellow, thickness)
+    cv2.line(img, (xn2, yn2), (xn4, yn4), yellow, thickness)
+    cv2.line(img, (xn4, yn4), (xn3, yn3), yellow, thickness)
+    cv2.line(img, (xn1, yn1), (xn3, yn3), yellow, thickness)
+
+    cv2.line(black, (xn1, yn1), (xn2, yn2), yellow, thickness)
+    cv2.line(black, (xn2, yn2), (xn4, yn4), yellow, thickness)
+    cv2.line(black, (xn4, yn4), (xn3, yn3), yellow, thickness)
+    cv2.line(black, (xn1, yn1), (xn3, yn3), yellow, thickness)
 
     cv2.imwrite(output_path + 'line_' + filename, img)
     cv2.imwrite(output_path + 'only_line_' + filename, black)
