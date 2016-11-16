ed446a354f2ca28298cff6a2b7b21fd711218693
Li Shunan <lishunan246@gmail.com>
2016-11-16 16:37:39 +0800

    求交修正

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from LineSegment import LineSegment, LineSegmentGroup
 from scipy import signal
 
 # 条形码比例3:2
-theta_tolerance=0.2
+theta_tolerance = 0.2
 DST_WIDTH = 300
 DST_HEIGHT = 200
 yellow = (0, 255, 255)
@@ -21,6 +21,9 @@ filenames = []
 # 所有被裁下的灰度图
 grays = []
 
+for filename in os.listdir(output_path):
+    os.remove(output_path+filename)
+
 for filename in os.listdir(input_path):
     filenames.append(filename)
     original = cv2.imread(input_path + filename, cv2.IMREAD_COLOR)
@@ -29,11 +32,13 @@ for filename in os.listdir(input_path):
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    edges = cv2.Canny(gray, 50, 150, apertureSize=3)
+    th, bw = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    edges = cv2.Canny(gray, th / 2, th)
+    # edges = cv2.Canny(gray, 50, 150, apertureSize=3)
     lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 100, minLineLength=80, maxLineGap=10)
-    if  lines is None:
-        print filename+' No line detected.'
-        continue 
+    if lines is None:
+        print filename + ' No line detected.'
+        continue
     line_group = []
     black = np.zeros((height, width, depth), np.uint8)
     for line in lines:
@@ -96,103 +101,122 @@ for filename in os.listdir(input_path):
     line_min = min(max_group, key=lambda x: x.d)
     line_max = max(max_group, key=lambda x: x.d)
 
-    x_avg = sum([ls.x for ls in max_group]) / (1.0 * len(max_group))
-    y_avg = sum([ls.y for ls in max_group]) / (1.0 * len(max_group))
-    xx = x_avg + 100
-    yy = y_avg + 100 * math.tan(avg_theta)
+    x_avgt = sum([ls.x for ls in max_group]) / (1.0 * len(max_group))
+    y_avgt = sum([ls.y for ls in max_group]) / (1.0 * len(max_group))
+    xx = x_avgt + 100
+    yy = y_avgt + 100 * math.tan(avg_theta)
 
-    new_line = LineSegment(x_avg, y_avg, xx, yy)
+    new_line = LineSegment(x_avgt, y_avgt, xx, yy)
     new_line.update(avg_theta)
     delta_d = (line_min.d + line_max.d) / 2.0 - new_line.d
+    # cv2.circle(black, (int(x_avgt), int(y_avgt)), 20, (255, 0, 255), -1)
+    xn = np.zeros((4, 2))
+    yn = np.zeros((4, 2))
+    dn = np.zeros(2)
 
-    x_avg += delta_d * math.cos(avg_theta + math.pi / 2.0)
-    y_avg += delta_d * math.sin(avg_theta + math.pi / 2.0)
+    for i in range(0, 2):
+        try:
+            if i == 0:
+                x_avg = x_avgt + delta_d * math.cos(avg_theta + math.pi / 2.0)
+                y_avg = y_avgt + delta_d * math.sin(avg_theta + math.pi / 2.0)
+                enlarge = (line_max.d - line_min.d) / 8.0
+            else:
+                x_avg = x_avgt - delta_d * math.cos(avg_theta + math.pi / 2.0)
+                y_avg = y_avgt - delta_d * math.sin(avg_theta + math.pi / 2.0)
+                enlarge = (line_max.d - line_min.d) / 8.0
 
-    cv2.circle(black, (int(x_avg), int(y_avg)), 30, (255, 255, 255), -1)
-    cv2.imwrite(output_path + 'theta_' + filename, black)
-    continue
-    #delta_d = (line_max.d - line_min.d) / 8.0
-    delta_d=0
-    x1, y1, x2, y2 = line_min.x1, line_min.y1, line_min.x2, line_min.y2
-    x1 -= delta_d * math.cos(avg_theta + math.pi / 2.0)
-    x2 -= delta_d * math.cos(avg_theta + math.pi / 2.0)
-    y1 -= delta_d * math.sin(avg_theta + math.pi / 2.0)
-    y2 -= delta_d * math.sin(avg_theta + math.pi / 2.0)
+            # print str(x_avg) + ' ' + str(y_avg)
+            cv2.circle(black, (int(x_avg), int(y_avg)), 10, (255, 255, 255), -1)
 
-    line_min = LineSegment(x1, y1, x2, y2)
+            cv2.putText(black, 'theta: ' + str(avg_theta) + 'd: ' + str(delta_d), (80, 300), font, 1, (255, 255, 255),
+                        2)
+            cv2.imwrite(output_path + 'theta_' + filename, black)
+            # continue
 
-    x1, y1, x2, y2 = line_max.x1, line_max.y1, line_max.x2, line_max.y2
-    x1 += delta_d * math.cos(avg_theta + math.pi / 2.0)
-    x2 += delta_d * math.cos(avg_theta + math.pi / 2.0)
-    y1 += delta_d * math.sin(avg_theta + math.pi / 2.0)
-    y2 += delta_d * math.sin(avg_theta + math.pi / 2.0)
+            enlarge = 0
+            x1, y1, x2, y2 = line_min.x1, line_min.y1, line_min.x2, line_min.y2
+            x1 -= enlarge * math.cos(avg_theta + math.pi / 2.0)
+            x2 -= enlarge * math.cos(avg_theta + math.pi / 2.0)
+            y1 -= enlarge * math.sin(avg_theta + math.pi / 2.0)
+            y2 -= enlarge * math.sin(avg_theta + math.pi / 2.0)
 
-    line_max = LineSegment(x1, y1, x2, y2)
+            line_min_new = LineSegment(x1, y1, x2, y2)
 
-    w = line_max.distance(line_min.x, line_min.y)
-    h = w / 3.0 * 2.1
-    r_2 = (w ** 2 + h ** 2) / 4.0
+            x1, y1, x2, y2 = line_max.x1, line_max.y1, line_max.x2, line_max.y2
+            x1 += enlarge * math.cos(avg_theta + math.pi / 2.0)
+            x2 += enlarge * math.cos(avg_theta + math.pi / 2.0)
+            y1 += enlarge * math.sin(avg_theta + math.pi / 2.0)
+            y2 += enlarge * math.sin(avg_theta + math.pi / 2.0)
 
-    l = line_min
-    x1, x2, y1, y2 = l.x1, l.x2, l.y1, l.y2
-    a = (y1 - y2) ** 2 + (x1 - x2) ** 2
-    b = 2.0 * ((y1 - y2) * (y1 - y_avg) + (x1 - x2) * (x1 - x_avg))
-    c = (y1 - y_avg) ** 2 + (x1 - x_avg) ** 2 - r_2
+            line_max_new = LineSegment(x1, y1, x2, y2)
 
-    t1 = (-b - math.sqrt(b ** 2 - 4.0 * a * c)) / (2.0 * a)
-    t2 = (-b + math.sqrt(b ** 2 - 4.0 * a * c)) / (2.0 * a)
+            w = line_max_new.distance(line_min_new.x, line_min_new.y)
+            h = w / 3.0 * 2.1
+            r_2 = (w ** 2 + h ** 2) / 4.0
 
-    xn1 = x1 + t1 * (x1 - x2)
-    yn1 = y1 + t1 * (y1 - y2)
+            l = line_min_new
+            x1, x2, y1, y2 = l.x1, l.x2, l.y1, l.y2
+            a = (y1 - y2) ** 2 + (x1 - x2) ** 2
+            b = 2.0 * ((y1 - y2) * (y1 - y_avg) + (x1 - x2) * (x1 - x_avg))
+            c = (y1 - y_avg) ** 2 + (x1 - x_avg) ** 2 - r_2
 
-    xn2 = x1 + t2 * (x1 - x2)
-    yn2 = y1 + t2 * (y1 - y2)
+            t1 = (-b - math.sqrt(b ** 2 - 4.0 * a * c)) / (2.0 * a)
+            t2 = (-b + math.sqrt(b ** 2 - 4.0 * a * c)) / (2.0 * a)
 
-    l = line_max
-    x1, x2, y1, y2 = l.x1, l.x2, l.y1, l.y2
-    a = (y1 - y2) ** 2 + (x1 - x2) ** 2
-    b = 2.0 * ((y1 - y2) * (y1 - y_avg) + (x1 - x2) * (x1 - x_avg))
-    c = (y1 - y_avg) ** 2 + (x1 - x_avg) ** 2 - r_2
+            xn[0, i] = x1 + t1 * (x1 - x2)
+            yn[0, i] = y1 + t1 * (y1 - y2)
 
-    t1 = (-b - math.sqrt(b ** 2 - 4.0 * a * c)) / (2.0 * a)
-    t2 = (-b + math.sqrt(b ** 2 - 4.0 * a * c)) / (2.0 * a)
+            xn[1, i] = x1 + t2 * (x1 - x2)
+            yn[1, i] = y1 + t2 * (y1 - y2)
 
-    xn3 = x1 + t1 * (x1 - x2)
-    yn3 = y1 + t1 * (y1 - y2)
+            l = line_max_new
+            x1, x2, y1, y2 = l.x1, l.x2, l.y1, l.y2
+            a = (y1 - y2) ** 2 + (x1 - x2) ** 2
+            b = 2.0 * ((y1 - y2) * (y1 - y_avg) + (x1 - x2) * (x1 - x_avg))
+            c = (y1 - y_avg) ** 2 + (x1 - x_avg) ** 2 - r_2
 
-    xn4 = x1 + t2 * (x1 - x2)
-    yn4 = y1 + t2 * (y1 - y2)
+            t1 = (-b - math.sqrt(b ** 2 - 4.0 * a * c)) / (2.0 * a)
+            t2 = (-b + math.sqrt(b ** 2 - 4.0 * a * c)) / (2.0 * a)
 
-    x_max = max_group[0].x1
-    x_min = max_group[len(max_group) - 1].x1
+            xn[2, i] = x1 + t1 * (x1 - x2)
+            yn[2, i] = y1 + t1 * (y1 - y2)
 
-    y_min = min(map(lambda x: x.y1, max_group))
-    y_max = max(map(lambda x: x.y2, max_group))
+            xn[3, i] = x1 + t2 * (x1 - x2)
+            yn[3, i] = y1 + t2 * (y1 - y2)
 
-    cv2.circle(black, (int(xn1), int(yn1)), 30, (255, 255, 255), -1)
-    cv2.circle(black, (int(xn2), int(yn2)), 30, (255, 255, 255), -1)
-    cv2.circle(black, (int(xn3), int(yn3)), 30, (255, 255, 255), -1)
-    cv2.circle(black, (int(xn4), int(yn4)), 30, (255, 255, 255), -1)
-    #  4 3
+            dn[i] = abs(math.sqrt((xn[0, i] - xn[1, i]) ** 2 + (yn[0, i] - yn[1, i]) ** 2) - math.sqrt(
+                (xn[2, i] - xn[3, i]) ** 2 + (yn[2, i] - yn[3, i]) ** 2))
+
+        except ValueError:
+            dn[i] = float('inf')
+
+    ni = 1 if dn[1] < dn[0] else 0
+
+    xn = xn[:, ni]
+    yn = yn[:, ni]
+
+    for i in range(0, 4):
+        cv2.circle(black, (int(xn[i]), int(yn[i])), 30, (255, 255, 255), -1)
+
+    # 4 3
     #  2 1
-    pts1 = np.float32([[xn2, yn2], [xn4, yn4], [xn1, yn1], [xn3, yn3]])
+    pts1 = np.float32([[xn[1], yn[1]], [xn[3], yn[3]], [xn[0], yn[0]], [xn[2], yn[2]]])
     pts2 = np.float32([[0, 0], [DST_WIDTH, 0], [0, DST_HEIGHT], [DST_WIDTH, DST_HEIGHT]])
     M = cv2.getPerspectiveTransform(pts1, pts2)
 
     dst = cv2.warpPerspective(original, M, (DST_WIDTH, DST_HEIGHT))
 
-    xn1, yn1, xn3, yn3, xn2, yn2, xn4, yn4 = int(xn1), int(yn1), int(xn3), int(yn3), int(xn2), int(yn2), int(xn4), int(
-        yn4)
+    xn = np.uint(xn)
+    yn = np.uint(yn)
+    cv2.line(img, (xn[0], yn[0]), (xn[1], yn[1]), yellow, thickness)
+    cv2.line(img, (xn[1], yn[1]), (xn[3], yn[3]), yellow, thickness)
+    cv2.line(img, (xn[3], yn[3]), (xn[2], yn[2]), yellow, thickness)
+    cv2.line(img, (xn[2], yn[2]), (xn[0], yn[0]), yellow, thickness)
 
-    cv2.line(img, (xn1, yn1), (xn2, yn2), yellow, thickness)
-    cv2.line(img, (xn2, yn2), (xn4, yn4), yellow, thickness)
-    cv2.line(img, (xn4, yn4), (xn3, yn3), yellow, thickness)
-    cv2.line(img, (xn1, yn1), (xn3, yn3), yellow, thickness)
-
-    cv2.line(black, (xn1, yn1), (xn2, yn2), yellow, thickness)
-    cv2.line(black, (xn2, yn2), (xn4, yn4), yellow, thickness)
-    cv2.line(black, (xn4, yn4), (xn3, yn3), yellow, thickness)
-    cv2.line(black, (xn1, yn1), (xn3, yn3), yellow, thickness)
+    cv2.line(black, (xn[0], yn[0]), (xn[1], yn[1]), yellow, thickness)
+    cv2.line(black, (xn[1], yn[1]), (xn[3], yn[3]), yellow, thickness)
+    cv2.line(black, (xn[3], yn[3]), (xn[2], yn[2]), yellow, thickness)
+    cv2.line(black, (xn[2], yn[2]), (xn[0], yn[0]), yellow, thickness)
 
     cv2.imwrite(output_path + 'line_' + filename, img)
     cv2.imwrite(output_path + 'only_line_' + filename, black)
